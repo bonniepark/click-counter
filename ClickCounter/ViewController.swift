526a1256253e5b488e6263bf39935d05790a907c
Bonnie Park <newapril.dev@gmail.com>
2021-01-24 15:01:43 +0900

    create the count and label properties

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // count and label properties
+    var count = 0
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,7 @@ class ViewController: UIViewController {
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         view.addSubview(label)
+        self.label = label
         
         // button
         let button = UIButton()
@@ -25,6 +30,11 @@ class ViewController: UIViewController {
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
+    }
+    
+    func incrementCount() {
+        self.count += 1
+        self.label.text = "\(count)"
     }
 
 
